13b58e84643a219cbe7256f146aff653853b066e
drinobre <dri_nobre@hotmail.com>
2022-01-15 16:41:59 +0000

    remove Item from the Array

--- index.js
+++ index.js
@@ -18,12 +18,22 @@ export function insertItem(array, item, index) {
 // Immutably replace an item at a specific position/index within an array
 export function replaceItem(array, item, index) {
   let newArray = [...array.slice(0, index), item, ...array.slice(index + 1)];
-  console.log(newArray);
+  // console.log(newArray);
   return newArray;
 }
 
+// const array = ["bread", "cheese", "ham", "bread"];
+// replaceItem(array, 2);
+
 // Immutably remove an item at a specific position/index within an array
-export function removeItem(array, index) {}
+export function removeItem(array, index) {
+  let newArray = [...array.slice(0, index), ...array.slice(index + 1)];
+  // console.log(newArray);
+  return newArray;
+}
+
+// const array = ["bread", "cheese", "ham", "bread"];
+// removeItem(array, 1);
 
 //Objects:
 
